07e2756201a726f98127b60d046db23b3e614789
SleepyCattStar <reallegend6759@gmail.com>
2026-07-25 23:22:00 +0530

    added ohlc candle

---
+++ include/analytics/ohlc.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include<chrono>
+#include<cstdint>
+#include<string>
+
+// OHLC-> open high low close
+
+struct OHLC{
+    std::string symbol;
+
+    double open = 0.0;
+    double high = 0.0;
+    double low = 0.0;
+    double close = 0.0;
+
+    double volume = 0.0;
+    std::uint64_t trade_count;
+
+    std::chrono::system_clock::time_point timestamp;
+
+};
